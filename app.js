529ef8daec319c50a717f42a69912fea05a1344f
Ian McKenney & Casey Heitz <student@epicodus.com>
2015-09-23 16:41:46 -0700

    Add upvote and downvote capability and sort by vote totals

--- app.js
+++ app.js
@@ -18,3 +18,23 @@ yellowbeacon.config(function($stateProvider, $urlRouterProvider) {
     controller: 'AnswersCtrl'
   });
 });
+
+// yellowbeacon.filter('orderObjectBy', function() {
+//   return function(input, attribute) {
+//     if(!angular.isObject(input)) {
+//       return input;
+//     }
+//
+//     var array = [];
+//     for(var objectKey in input) {
+//       array.push(input[objectKey]);
+//     }
+//
+//     array.sort(function(a, b) {
+//       a = parseInt(a[attribute]);
+//       b = parseInt(b[attribute]);
+//       return a - b;
+//     });
+//     return array;
+//   }
+// });
--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -1,7 +1,24 @@
 yellowbeacon.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, QuestionsFactory, UtilitiesFactory) {
   $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId);
   $scope.addAnswer = function() {
-    $scope.question.answers.push({ text: $scope.answerText, comments: [], upvotes: $scope.upvoteCount, downvotes: $scope.downvoteCount });
+    $scope.question.answers.push({ text: $scope.answerText, comments: [], upvotes: 0, downvotes: 0, voteTotal: 0 });
     $scope.answerText = null;
   };
+
+  $scope.addUpvote = function(answer) {
+    var index = $scope.question.answers.indexOf(answer);
+    $scope.question.answers[index].upvotes  += 1;
+    $scope.question.answers[index].voteTotal  += 1;
+  };
+
+  $scope.addDownvote = function(answer) {
+    var index = $scope.question.answers.indexOf(answer);
+    $scope.question.answers[index].downvotes += 1;
+    $scope.question.answers[index].voteTotal  -= 1;
+  };
+  //
+  // $scope.voteTotal = function(answer) {
+  //   var index = $scope.question.answers.indexOf(answer);
+  //   return $scope.question.answers[index].upvotes - $scope.question.answers[index].downvotes;
+  // };
 });
